df229cc180c2404f06d6936f5d9b8c42f97bb765
Jakko Turro <jakkoturro@gmail.com>
2025-10-08 16:59:51 +0300

    Changed a comment

--- app/src/main/java/ee/ut/cs/iotbazaar/model/Item.kt
+++ app/src/main/java/ee/ut/cs/iotbazaar/model/Item.kt
@@ -3,9 +3,7 @@ package ee.ut.cs.iotbazaar.model
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-/**
- * Room entity representing an IoT item that a user can reserve/borrow.
- */
+// Room entity representing an IoT item that a user can reserve/borrow.
 @Entity(tableName = "item")
 data class Item(
     @PrimaryKey(autoGenerate = true) val id: Long = 0,
